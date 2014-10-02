00dafe491e0d0b59fa53e86d8c187c42d7824200
Adrian Mejia <adriansky@gmail.com>
2014-10-02 13:49:37 -0400

    Update: PUT /todos/:id

--- routes/todos.js
+++ routes/todos.js
@@ -28,4 +28,12 @@ router.get('/:id', function(req, res, next) {
   });
 });
 
+/* PUT /todos/:id */
+router.put('/:id', function(req, res, next) {
+  Todo.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
 module.exports = router;
