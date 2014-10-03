39f04b716a29b928bc4c8b7d961cab9e9502bce6
Adrian Mejia <adriansky@gmail.com>
2014-10-03 19:46:39 -0400

    default values

--- models/Todo.js
+++ models/Todo.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var TodoSchema = new mongoose.Schema({
   name: String,
   completed: Boolean,
-  note: String
+  note: { type: String, default: '' },
+  updated_at: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Todo', TodoSchema);
