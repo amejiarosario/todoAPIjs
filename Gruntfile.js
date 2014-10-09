04076337368e1b44983970c50303f601f6a078d1
Adrian Mejia <adriansky@gmail.com>
2014-10-09 01:31:20 -0400

    watch and concurrent

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,8 @@
 module.exports = function(grunt){
   var paths = {
     frontjs: ['public/app/*.js', 'public/app/**/*.js'],
-    backjs: ['routes/*.js', 'models/*.js', 'app.js']
+    backjs: ['routes/*.js', 'models/*.js', 'app.js'],
+    html: ['public/app/**/*.html'],
   };
 
   grunt.config.init({
@@ -16,15 +17,47 @@ module.exports = function(grunt){
         src: paths.backjs
       }
     },
+
     nodemon: {
+      options: {
+        watch: paths.backjs,
+        callback: function (nodemon) {
+          nodemon.on('log', function (event) {
+            console.log(event.colour);
+          });
+
+          nodemon.on('restart', function () {
+            require('fs').writeFileSync('.rebooted', 'rebooted');
+          });
+        }
+      },
       dev: {
         script: './bin/www'
-      }
+      },
+    },
+
+    watch: {
+      options: {
+        spawn: false,
+        livereload: true,
+      },
+      scripts: {
+        files: paths.frontjs.concat(paths.html).concat(['.rebooted']),
+        tasks: ['jshint'],
+      },
+    },
+    concurrent: {
+      options: {
+        logConcurrentOutput: true
+      },
+      target: ['nodemon', 'watch']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-concurrent');
 
-  grunt.registerTask('default', ['jshint', 'nodemon']);
+  grunt.registerTask('default', ['jshint', 'concurrent']);
 }
