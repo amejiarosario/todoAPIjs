f801134c1efeab4d36a5d3f05633500856dbdfac
Adrian Mejia <admejiar@cisco.com>
2016-08-06 17:10:21 -0400

    List: GET /todos

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var todos = require('./routes/todos');
 
 // load mongoose package
 var mongoose = require('mongoose');
@@ -35,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/todos', todos);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
