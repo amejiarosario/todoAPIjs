2b8427bb08036a09d14a4a9165ee87aa80984619
Adrian Mejia <admejiar@cisco.com>
2016-08-06 20:30:58 -0400

    make ejs the view template

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
